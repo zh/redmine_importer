30472b22115a0546f1056219ce76b3c588114ecd
mruzicka <michal.ruzicka@cloudsmith.com>
2012-11-24 20:08:21 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 match '/importer/index', :to => 'importer#index', :via => [:get, :post]
-match '/importer/index', :to => 'importer#match', :via => [:get, :post]
-match '/importer/index', :to => 'importer#result', :via => [:get, :post]
+match '/importer/match', :to => 'importer#match', :via => [:get, :post]
+match '/importer/result', :to => 'importer#result', :via => [:get, :post]
