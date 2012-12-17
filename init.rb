667e20cd460030882a01de3520c5988deb80d8af
stoyan <stoyan@kndb.jp>
2012-12-18 00:01:43 +0900

    ruby-1.9 and redmine-2.2 ready

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_importer do
   name 'Issue Importer'
-  author 'Martin Liu / Leo Hourvitz'
+  author 'Martin Liu / Leo Hourvitz / Stoyan Zhekov'
   description 'Issue import plugin for Redmine.'
-  version '1.0'
+  version '1.1'
 
   project_module :importer do
     permission :import, :importer => :index
