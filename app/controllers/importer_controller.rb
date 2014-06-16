zh/redmine_importer

commit 58e43592546fc02a2dd9d363254464a67e3ae2e2

Support list field and bugfix.

app/controllers/importer_controller.rb
@@ -268,7 +268,7 @@ class ImporterController < ApplicationController
         end
       end
     end
-    if unique_error && unique_attr == nil
+    if unique_error && unique_attr.blank?
       flash[:error] = unique_error
       return
     end
@@ -400,7 +400,7 @@ class ImporterController < ApplicationController
       # parent issues
       begin
         parent_value = row[attrs_map["parent_issue"]]
-        if parent_value && (parent_value.length > 0)
+        if parent_value.present?
           issue.parent_issue_id = issue_for_unique_attr(unique_attr,parent_value,row).id
         end
       rescue NoIssueForUniqueValue
@@ -422,7 +422,8 @@ class ImporterController < ApplicationController
       # custom fields
       custom_failed_count = 0
       issue.custom_field_values = issue.available_custom_fields.inject({}) do |h, cf|
-        if value = row[attrs_map[cf.name]].blank? ? nil : row[attrs_map[cf.name]]
+        value = row[attrs_map[cf.name]]
+        if value.present?
           begin
             if cf.field_format == 'user'
               value = user_id_for_login!(value).to_s
@@ -430,6 +431,8 @@ class ImporterController < ApplicationController
               value = version_id_for_name!(project,value,add_versions).to_s
             elsif cf.field_format == 'date'
               value = value.to_date.to_s(:db)
+            elsif cf.field_format == 'list'
+              value = value.split(',').map(&:strip)
             end
             h[cf.id] = value
           rescue

commit fad0dfdf279c7d03628aa9ffce9c3407380ed5f3

regression brought by commit 58e43592546fc02a2dd9d363254464a67e3ae2e2 that supposes that a list custom field has always multiple values 	modifié:         app/controllers/importer_controller.rb

app/controllers/importer_controller.rb
@@ -433,7 +433,7 @@ class ImporterController < ApplicationController
               value = version_id_for_name!(project,value,add_versions).to_s
             elsif cf.field_format == 'date'
               value = value.to_date.to_s(:db)
-            elsif cf.field_format == 'list'
+            elsif cf.multiple? && cf.field_format == 'list'
               value = value.split(',').map(&:strip)
             elsif cf.field_format == 'text'
               value = value.gsub(/\r\n?|\\n|<br\s*\/?>/, "\n")